6b1e5837abe445b1837ab130c2bda5182b99f7e3
Adedipupo <bayonleolaluwa@gmail.com>
2020-07-18 13:25:20 +0100

    js logic

--- app.js
+++ app.js
@@ -3,21 +3,6 @@ let button = document.getElementById("myBtn");
 let ul = document.querySelector("ul");
 let item = document.getElementsByTagName("li");
 
-function inputLength() {
-    return input.value.length;
-} 
-
-function createElement(){
-    let li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li);
-    input.value="";
-}
-
-function addListAFterClick(){
-    if (input.value.length > 0){
-        createElement();
-    }
-}
-
-input.addEventListener("click", addListAFterClick);
+input.addEventListener("click", function(){
+    
+})
