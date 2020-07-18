5b4e1420b290315200eb27ec9a5a2ac151ab2353
Adedipupo <bayonleolaluwa@gmail.com>
2020-07-18 13:42:28 +0100

    js logic

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let input = document.getElementById("myInput");
 let button = document.getElementById("myBtn");
 let ul = document.querySelector("ul");
 
-input.addEventListener("click", function(){
+button.addEventListener("click", function(){
     if(input.value.length > 0) {
         let li = document.createElement("li");
         li.appendChild(document.createTextNode(input.value));
