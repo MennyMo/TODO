71562de4149da79e53f7157181f6f3edd5a028ce
Adedipupo <bayonleolaluwa@gmail.com>
2020-07-18 12:12:01 +0100

    storing data using DOM

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 let input = document.getElementById("myInput");
 let ul = document.querySelector("ul");
 let item = document.getElementsByTagName("li");
+let button = document.getElementsById("myBtn");
